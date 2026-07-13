3025256bfc5f71b780b5aee52285b9991d48fefd
Saud Iqbal <6971514+saudiqbal@users.noreply.github.com>
2026-07-13 18:40:22 -0400

    Quick Fix

--- setup.php
+++ setup.php
@@ -1,6 +1,44 @@
 <?php
+// Login Password
+$password='';
+
+$fresh='1';
+if($fresh=='1')
+{
+	// Password Generator
+	$chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	$count = strlen($chars);
+	$bytes = random_bytes(32);
+	$result = '';
+	foreach (str_split($bytes) as $byte) {
+	$result .= $chars[ord($byte) % $count];
+	}
+	// Change variables
+	$filePath = 'setup.php';
+	$content = file_get_contents($filePath);
+	// Map variable names to their exact new string values
+	$replacements = [
+	'password' => $result,
+	'fresh' => '0'
+	];
+	// Build regex patterns to find '$variable_name = ...' and replace values
+	foreach ($replacements as $varName => $newValue) {
+		// Regex looks for $varName followed by optional spaces, an =, optional spaces, and the quoted value
+		$pattern = '/(\$' . preg_quote($varName, '/') . '\s*=\s*[\'"])(.*?)([\'"];)/';
+		$content = preg_replace($pattern, '${1}' . $newValue . '${3}', $content);
+	}
+// Update the configuration file safely
+file_put_contents($filePath, $content);
+}
 $error = '1';
-$message = '<div style="text-align: center;">Create New SQLite Database</div><div style="text-align: center;margin: 50px;"><a href="setup.php?setup=true" style="color:#000;background-color: #f44336;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;">Continue</a> <a href="./" style="color:#000;background-color: #4CAF50;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;">Cancel</a></div>';
+$login = '0';
+if(isset($_GET['password']) && $password == $_GET['password'] && !empty($_GET['password']))
+{
+$login = '1';
+}
+if($login == '1')
+{
+$message = '<div style="text-align: center;">Create New SQLite Database</div><div style="text-align: center;margin: 50px;"><a href="setup.php?password='.$password.'&setup=true" style="color:#000;background-color: #f44336;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;">Continue</a> <a href="./" style="color:#000;background-color: #4CAF50;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;">Cancel</a></div>';
 if (isset($_GET['setup']))
 {
 	$setup = $_GET['setup'];
@@ -9,7 +47,7 @@ if (isset($_GET['setup']))
 		require_once __DIR__ . '/config.php';
 		if (file_exists($db_filename))
 		{
-			$message = "<div style=\"text-align: center;margin: 25px;\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\"><path style=\"fill:#ff5500\" d=\"M19.64 16.36 11.53 2.3A1.85 1.85 0 0 0 10 1.21 1.85 1.85 0 0 0 8.48 2.3L.36 16.36C-.48 17.81.21 19 1.88 19h16.24c1.67 0 2.36-1.19 1.52-2.64zM11 16H9v-2h2zm0-4H9V6h2z\"/></svg> SQLite Database file already exists.</div><div style=\"text-align: center;margin: 25px;\">You can delete the existing SQLite Database file and recreate the database, you will lose all your existing data forever if you choose to continue.</div><div style=\"text-align: center;margin: 50px;\"><a href=\"setup.php?setup=true&delete=true\" style=\"color:#000;background-color: #f44336;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;\">Delete and continue</a> <a href=\"./\" style=\"color:#000;background-color: #4CAF50;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;\">Cancel</a></div>";
+			$message = "<div style=\"text-align: center;margin: 25px;\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\"><path style=\"fill:#ff5500\" d=\"M19.64 16.36 11.53 2.3A1.85 1.85 0 0 0 10 1.21 1.85 1.85 0 0 0 8.48 2.3L.36 16.36C-.48 17.81.21 19 1.88 19h16.24c1.67 0 2.36-1.19 1.52-2.64zM11 16H9v-2h2zm0-4H9V6h2z\"/></svg> SQLite Database file already exists.</div><div style=\"text-align: center;margin: 25px;\">You can delete the existing SQLite Database file and recreate the database, you will lose all your existing data forever if you choose to continue.</div><div style=\"text-align: center;margin: 50px;\"><a href=\"setup.php?password=$password&setup=true&delete=true\" style=\"color:#000;background-color: #f44336;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;\">Delete and continue</a> <a href=\"./\" style=\"color:#000;background-color: #4CAF50;padding: 10px 24px;border-radius: 4px;cursor: default;text-decoration: none;\">Cancel</a></div>";
 			$error = '1';
 			if (isset($_GET['delete']))
 			{
@@ -42,6 +80,13 @@ if (isset($_GET['setup']))
 			}
 		}
 	}
+}
+}
+else
+{
+	$message = '<div style="display: flex;justify-content: center;align-items: center;"><form method="GET" id="login">
+<div class="login-group"><div class="login-group-prepend"><span class="login-group-text"><input id="password" name="password" type="password" style="border: none; outline: none;" placeholder=\'Password\' required></span></div><input type="submit" name="submit" id="submit" value="submit" class="login-control"></div></div>
+</form></div>';
 }
 ?>
 <!DOCTYPE html>
@@ -112,6 +157,70 @@ box-shadow: 0 0 25px #FF9633;
 -webkit-border-radius: 0px 0px 5px 5px;
 -moz-border-radius: 0px 0px 5px 5px;
 border-radius: 0px 0px 5px 5px;
+}
+.login-group {
+position: relative;
+display: flex;
+flex-wrap: wrap;
+align-items: stretch;
+font-family: monospace;
+}
+.login-group-prepend {
+margin-right: -1px;
+}
+.login-group>.login-control:not(:first-child), .login-group>.custom-select:not(:first-child) {
+border-top-left-radius: 0;
+border-bottom-left-radius: 0;
+}
+.login-group>.login-control, .login-group>.login-control-plaintext, .login-group>.custom-select, .login-group>.custom-file {
+position: relative;
+width: fit-content;
+min-width: 0;
+margin-bottom: 0;
+}
+.login-group>*, *::before, *::after {
+box-sizing: border-box;
+}
+.login-group>.login-group-prepend>.btn, .login-group>.login-group-prepend>.login-group-text, .login-group:not(.has-validation)>.login-group-append:not(:last-child)>.btn, .login-group:not(.has-validation)>.login-group-append:not(:last-child)>.login-group-text, .login-group.has-validation>.login-group-append:nth-last-child(n+3)>.btn, .login-group.has-validation>.login-group-append:nth-last-child(n+3)>.login-group-text, .login-group>.login-group-append:last-child>.btn:not(:last-child):not(.dropdown-toggle), .login-group>.login-group-append:last-child>.login-group-text:not(:last-child) {
+border-top-right-radius: 0;
+border-bottom-right-radius: 0;
+}
+.login-group-text {
+display: flex;
+align-items: center;
+padding: 0px;
+margin-bottom: 0;
+font-size: 1rem;
+font-weight: 400;
+line-height: 1;
+text-align: center;
+white-space: nowrap;
+color: #495057;
+background-color: #f8f9fa;
+border: 1px solid #ced4da;
+border-radius: .25rem;
+}
+.login-control:hover {
+background-color: #79beff;
+color: #1e1e1e;
+}
+.login-control {
+display: block;
+width: 50px;
+height: calc(1em + 0.75rem + 1px);
+padding: .375rem .75rem;
+font-size: 1rem;
+font-weight: 400;
+line-height: 1;
+color: #495057;
+background-color: #e9ecef;
+background-clip: padding-box;
+border: 1px solid #ced4da;
+border-radius: .25rem;
+transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
+.login-group-prepend, .login-group-append {
+display: flex;
 }
 </style>
 </head>
