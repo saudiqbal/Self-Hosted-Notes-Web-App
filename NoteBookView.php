065906ac6b74d33d8f5f4d29c46c3f960770008b
Saud Iqbal <6971514+saudiqbal@users.noreply.github.com>
2026-07-27 21:05:57 -0400

    Fix

--- NoteBookView.php
+++ NoteBookView.php
@@ -82,7 +82,7 @@ exit;
 <a href="./NoteBookDelete.php?NoteBookDelete=<?php echo $NoteBookView;?>">Notebook Delete</a>
 </div>
 </div>
-<a href="./NoteAdd.php?NoteBookID=<?php echo $NoteBookView;?>">Add New Note <svg viewBox="0 0 24 24" width="20" height="20" style="vertical-align: middle;line-height: 20px;" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg></a>
+<a href="./NoteAdd.php?NoteBookID=<?php echo $NoteBookView;?>">Add New Note <svg viewBox="0 0 24 24" width="20" height="20" style="vertical-align: middle;line-height: 20px;transform: translateY(-1px);" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg></a>
 </span>
 </div>
 </div>
